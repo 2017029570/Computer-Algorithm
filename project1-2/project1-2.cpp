3867d476c08e4849f1020bf9cb9ab9c47b9c2e25
2017029570 <2017029570@hanyang.ac.kr>
2018-10-05 00:50:41 -0700

    project1-2.cpp

--- project1-2/project1-2.cpp
+++ project1-2/project1-2.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int* MergeSort(int* arr, int* arr2, int size, int m) {
 		int* answer = new int[m+size];
 		
-//		for(int i=0;i<size;i++) printf("%d ",arr2[i]);
 		if(size == 0) {
 				for(int i=0;i<m;i++) answer[i] = arr[i];
 		}
@@ -27,7 +26,6 @@ int* MergeSort(int* arr, int* arr2, int size, int m) {
 								j++;
 						}
 						k++;
-//						printf("i : %d j : %d\n",i,j);
 				}
 
 				if(j<size) {
@@ -45,9 +43,6 @@ int* MergeSort(int* arr, int* arr2, int size, int m) {
 				}
 		}
 
-//		for(int i=0;i<size;i++) printf("%d ",arr2[i]);
-//		for(int b=0;b<size+m;b++) printf("%d ",answer[b]);
-//		printf("\n");
 
 		return answer;
 }
@@ -77,40 +72,16 @@ int main(int argc, char* argv[]) {
 						i++;
 						token = strtok(NULL," ");
 				};
-				int* print = new int[m+size];
-				print = MergeSort(arr,answer,size,m);
-				answer = print;
-	//			for(int a=0;a<size+m;a++) printf("%d ",print[a]);
-	//			printf("\n");
+				answer = MergeSort(arr, answer, size, m);
 				size+=m;
-			//	free(print);
 				fclose(f);
 		}
 
-//		for(int a=0;a<size+m;a++) printf("%d ",answer[a]);
-	/*	int out[size/k];
-		int b = k-1;
-		for(int a=0;a<size/k;a++) {
-				out[a] = answer[b];
-						b+=k;
-				}
-	*/
-	//	string out = "";
-	//	for(int a=0;a<size/k;a++) out+=to_string(answer[a]);
-	//	cout<<out<<endl;
 		FILE* fo = fopen("output.txt","w");
-	//	fwrite(out,sizeof(char), size/k,fo);
-	//	for(int i=0;i<size/k;i++) putw(out[i],fo);
 		for(int a=0;a<size/k;a++)
 			fprintf(fo, "%d ",answer[a]);
 		
 		fclose(fo);
 
-	/*	int bff[100001];
-		fo = fopen("output.txt","rb");
-		fread(bff,sizeof(int),size/k,fo);
-		for(int i=0;i<size/k;i++) printf("%d ",bff[i]);
-		printf("\n");
-		fclose(fo);*/
 
 }
